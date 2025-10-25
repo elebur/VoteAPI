03f84cedc2c3d7e36e42a45e67c00f43955a0706
elebur <elebur249@proton.me>
2025-10-25 15:07:54 +0300

    feat: add an endpoint for creating emplyees

--- company/api/urls.py
+++ company/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views.employee import get_employee
+from api.views.employee import get_employee, add_employee
 
 urlpatterns = [
-    path("employee/<int:pk>/", get_employee, name="get_employee")
+    path("employee/<int:pk>/", get_employee, name="get_employee"),
+    path("employee/", add_employee, name="add_employee"),
 ]
--- company/api/views/employee.py
+++ company/api/views/employee.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework import status
 
 from base.models import Employee
 from api.serializers import EmployeeSerializer
@@ -12,3 +13,18 @@ def get_employee(request: Request, pk: int):
     e = get_object_or_404(Employee, pk=pk)
     serializer = EmployeeSerializer(e, many=False)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_employee(request: Request):
+    serializer = EmployeeSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        status_code = status.HTTP_201_CREATED
+        result = {"employee_id": serializer.data["id"]}
+    else:
+        status_code = status.HTTP_400_BAD_REQUEST
+        result = {"details": serializer.errors}
+
+    return Response(result, status=status_code)
