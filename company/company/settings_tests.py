37af974676e284f9db6aa3b1181ba459118f9b4a
elebur <elebur249@proton.me>
2025-10-26 16:14:01 +0200

    refactor: set SQLITE DB for test server

--- company/company/settings_tests.py
+++ company/company/settings_tests.py
@@ -74,8 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'company.wsgi.application'
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -135,16 +133,10 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
