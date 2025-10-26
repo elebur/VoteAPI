e6df0a5a468f13c977e5167c3451a67b549db548
elebur <elebur249@proton.me>
2025-10-26 14:45:28 +0200

    tests(feat): add 'de_json'

--- company/api/tests/tools.py
+++ company/api/tests/tools.py
@@ -3,6 +3,10 @@ import json
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
+def de_json(text: str) -> dict:
+    return json.loads(text)
+
+
 def get_jwt_for_user(user):
     refresh = RefreshToken.for_user(user)
     return {
