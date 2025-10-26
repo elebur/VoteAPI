8464b2ee917dff317dfe31bd5bb5c6831f71d663
elebur <elebur249@proton.me>
2025-10-26 14:44:15 +0200

    test(refactor): remove redundant helper function

--- company/api/tests/tools.py
+++ company/api/tests/tools.py
@@ -1,10 +1,4 @@
 import json
 
-from django.test import Client
 
 
-def send_json(client: Client, url: str, payload: dict):
-    resp = client.post(url,
-                       json.dumps(payload),
-                       content_type="application/json")
-    return json.loads(resp.text)
