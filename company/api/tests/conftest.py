a5833ee8f71dc2c190c8123e7e81cefdc627b32c
elebur <elebur249@proton.me>
2025-10-28 19:10:26 +0200

    tests: add tests for the menu endpoint

--- company/api/tests/conftest.py
+++ company/api/tests/conftest.py
@@ -1,11 +1,13 @@
+from datetime import timedelta
 import sys
 from pathlib import Path
 
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from rest_framework.test import APIClient
 import pytest
 
-from base.models import Employee, Restaurant
+from base.models import Employee, Restaurant, Menu
 
 
 sys.path.append(Path(__file__).resolve().parent)  # type:ignore
@@ -51,6 +53,48 @@ def employee(db):
 @pytest.fixture
 def restaurant(db):
     return Restaurant.objects.create(name="Restaurant")
+
+
+@pytest.fixture
+def menu(db):
+    restaurant = Restaurant.objects.create(name="Restaurant For Menu Fixture")
+    menu = Menu.objects.create(restaurant=restaurant,
+                               launch_date=timezone.now().date())
+    menu.items.create(restaurant=restaurant,
+                      title="Menu item", description="Description")
+
+    return menu
+
+
+@pytest.fixture
+def multiple_menus(db):
+    r1 = Restaurant.objects.create(name="Restaurant#1")
+    r2 = Restaurant.objects.create(name="Restaurant#2")
+    r3 = Restaurant.objects.create(name="Restaurant#3")
+    r4 = Restaurant.objects.create(name="Restaurant#4")
+
+    today = timezone.now().date()
+    menu1 = Menu.objects.create(restaurant=r1, launch_date=today)
+    menu2 = Menu.objects.create(restaurant=r2, launch_date=today - timedelta(days=2))
+    menu3 = Menu.objects.create(restaurant=r3, launch_date=today)
+    menu4 = Menu.objects.create(restaurant=r4, launch_date=today)
+
+    menu1.items.create(restaurant=r1, title="Item#1_1", description="Descr#1_1")
+    menu1.items.create(restaurant=r1, title="Item#1_2", description="Descr#1_2")
+
+    menu2.items.create(restaurant=r2, title="Item#2_1", description="Descr#2_1")
+
+    menu3.items.create(restaurant=r3, title="Item#2_1", description="Descr#3_1")
+    menu3.items.create(restaurant=r3, title="Item#3_2", description="Descr#3_2")
+
+    menu4.items.create(restaurant=r4, title="Item#4_1", description="Descr#4_1")
+    menu4.items.create(restaurant=r4, title="Item#4_2", description="Descr#4_2")
+    menu4.items.create(restaurant=r4, title="Item#4_3", description="Descr#4_3")
+
+    return menu
+
+
+
 
 
 @pytest.fixture
--- company/api/tests/test_menu.py
+++ company/api/tests/test_menu.py
@@ -1,82 +1,261 @@
-def test_create_by_admin():
-    assert 1 == 2
+from types import MappingProxyType
+from copy import deepcopy
+
+import pytest
+
+from api.tests.tools import auth_client, de_json, get_jwt_for_user, AUTH_REQUIRED_401
 
 
-def test_create_by_user():
-    assert 1 == 2
+MENU_ITEMS = (
+    {
+        "title": "Bruschetta al Pomodoro",
+        "description": "Toasted bread topped with fresh tomatoes, basil, and olive oil."
+    },
+    {
+        "title": "Risotto ai Funghi Porcini",
+        "description": "Creamy risotto with porcini mushrooms and Parmesan cheese."
+    },
+    {
+        "title": "Spaghetti Carbonara",
+        "description": "Classic pasta with egg, pancetta, and Pecorino Romano cheese."
+    },
+    {
+        "title": "Lasagna alla Bolognese",
+        "description": "Layered pasta with Bolognese meat sauce and béchamel cream."
+    },
+    {
+        "title": "Tiramisu",
+        "description": "Traditional Italian dessert made with mascarpone, coffee, and cocoa."
+    }
+)
+
+MENU_REQUEST_BODY = {
+    "restaurant": 1,
+    "title": "Italian",
+    "notes": "Monday's menu",
+    "launch_date": "2025-10-26",
+    "items": MENU_ITEMS
+}
+
+MULTIPLE_MENUS_STRUCTURE = [
+    {
+        'id': 1,
+        'items': [
+            {'id': 1, 'title': 'Item#1_1', 'description': 'Descr#1_1'},
+            {'id': 2, 'title': 'Item#1_2', 'description': 'Descr#1_2'}],
+        'title': None,
+        'notes': None,
+        'launch_date': '2025-10-16',
+        'date_created': '2025-10-16T19:00:00Z',
+        'last_modified': '2025-10-16T19:00:00Z',
+        'restaurant': 1
+    },
+    {
+        'id': 3,
+        'items': [
+            {'id': 4, 'title': 'Item#2_1', 'description': 'Descr#3_1'},
+            {'id': 5, 'title': 'Item#3_2', 'description': 'Descr#3_2'}],
+        'title': None,
+        'notes': None,
+        'launch_date': '2025-10-16',
+        'date_created': '2025-10-16T19:00:00Z',
+        'last_modified': '2025-10-16T19:00:00Z',
+        'restaurant': 3
+    },
+    {
+        'id': 4,
+        'items': [
+            {'id': 6, 'title': 'Item#4_1', 'description': 'Descr#4_1'},
+            {'id': 7, 'title': 'Item#4_2', 'description': 'Descr#4_2'},
+            {'id': 8, 'title': 'Item#4_3', 'description': 'Descr#4_3'}],
+        'title': None,
+        'notes': None,
+        'launch_date': '2025-10-16',
+        'date_created': '2025-10-16T19:00:00Z',
+        'last_modified': '2025-10-16T19:00:00Z',
+        'restaurant': 4
+    }]
+
+ENDPOINT = "/api/menu/"
 
 
-def test_create_by_anonymous():
-    assert 1 == 2
+def test_create_by_admin(admin, client, restaurant):
+    client = auth_client(client, get_jwt_for_user(admin))
+
+    resp = client.post(ENDPOINT, MENU_REQUEST_BODY)
+
+    assert resp.status_code == 201
+    assert resp.text == '{"menu_id":1}'
 
 
-def test_create_with_one_item():
-    assert 1 == 2
+def test_create_by_user(user, client, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+
+    resp = client.post(ENDPOINT, MENU_REQUEST_BODY)
+
+    assert resp.text == '{"menu_id":1}'
+    assert resp.status_code == 201
 
 
-def test_create_with_multiple_items():
-    assert 1 == 2
+def test_create_by_anonymous(client, restaurant):
+
+    resp = client.post(ENDPOINT, MENU_REQUEST_BODY)
+
+    assert resp.text == AUTH_REQUIRED_401
+    assert resp.status_code == 401
 
 
-def test_create_without_items():
-    assert 1 == 2
+def test_create_without_items(user, client, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload["items"] = list()
+
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '''{"details":"'items' is the required parameter. It can\'t be null or an empty array"}'''
+    assert resp.status_code == 400
 
 
-def test_create_with_item_without_title():
-    assert 1 == 2
+def test_create_with_item_without_title(client, user, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload["items"][0].pop("title")
+
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"details":{"items":[{"title":["This field is required."]},{},{},{},{}]}}'
+    assert resp.status_code == 400
 
 
-def test_create_with_item_without_description():
-    assert 1 == 2
+def test_create_with_item_without_description(client, user, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload["items"][0].pop("description")
+
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"details":{"items":[{"description":["This field is required."]},{},{},{},{}]}}'
+    assert resp.status_code == 400
 
 
-def test_create_without_restaurant():
-    assert 1 == 2
+def test_admin_creates_menu_without_restaurant(admin, client, restaurant):
+    client = auth_client(client, get_jwt_for_user(admin))
+
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload.pop("restaurant")
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"details":{"restaurant":["This field is required."]}}'
+    assert resp.status_code == 400
 
 
-def test_create_without_title():
-    assert 1 == 2
+def test_admin_creates_menu_with_non_existing_restaurant(admin, client, restaurant):
+    client = auth_client(client, get_jwt_for_user(admin))
+
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload["restaurant"] = 9999
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"details":{"restaurant":["Invalid pk \\"9999\\" - object does not exist."]}}'
+    assert resp.status_code == 400
 
 
-def test_create_without_notes():
-    assert 1 == 2
+def test_create_without_title(client, user, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload.pop("title")
+
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"menu_id":1}'
+    assert resp.status_code == 201
 
 
-def test_create_without_launch_date():
-    assert 1 == 2
+def test_create_without_notes(client, user, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload.pop("notes")
+
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"menu_id":1}'
+    assert resp.status_code == 201
 
 
-def test_retrieve_by_user():
-    assert 1 == 2
+def test_create_without_launch_date(client, user, restaurant):
+    client = auth_client(client, get_jwt_for_user(user))
+    payload = deepcopy(MENU_REQUEST_BODY)
+    payload.pop("launch_date")
+
+    resp = client.post(ENDPOINT, payload)
+
+    assert resp.text == '{"details":{"launch_date":["This field is required."]}}'
+    assert resp.status_code == 400
 
 
-def test_retrieve_by_admin():
-    assert 1 == 2
+@pytest.mark.freeze_time("2025-10-16T11:00:00Z")
+def test_retrieve_by_id_by_user(client, user, restaurant, menu):
+    client = auth_client(client, get_jwt_for_user(user))
+    resp = client.get(ENDPOINT + f"{menu.id}/")
+
+    assert resp.text == ('{"id":1,"items":[{"id":1,"title":"Menu item","description":"Description"}],"title":null,"notes":null,"launch_date":"2025-10-16","date_created":"2025-10-16T11:00:00Z","last_modified":"2025-10-16T11:00:00Z","restaurant":2}')
+    assert resp.status_code == 200
 
 
-def test_retrieve_by_anonymous():
-    assert 1 == 2
+@pytest.mark.freeze_time("2025-10-16T11:00:00Z")
+def test_retrieve_by_id_by_admin(client, admin, restaurant, menu):
+    client = auth_client(client, get_jwt_for_user(admin))
+    resp = client.get(ENDPOINT + f"{menu.id}/")
+
+    assert resp.text == '{"id":1,"items":[{"id":1,"title":"Menu item","description":"Description"}],"title":null,"notes":null,"launch_date":"2025-10-16","date_created":"2025-10-16T11:00:00Z","last_modified":"2025-10-16T11:00:00Z","restaurant":2}'
+    assert resp.status_code == 200
 
 
-def test_retrieve_non_existing():
-    assert 1 == 2
+@pytest.mark.freeze_time("2025-10-16T11:00:00Z")
+def test_retrieve_by_id_by_anonymous(client, restaurant, menu):
+    resp = client.get(ENDPOINT + f"{menu.id}/")
+
+    assert resp.text == '{"id":1,"items":[{"id":1,"title":"Menu item","description":"Description"}],"title":null,"notes":null,"launch_date":"2025-10-16","date_created":"2025-10-16T11:00:00Z","last_modified":"2025-10-16T11:00:00Z","restaurant":2}'
+    assert resp.status_code == 200
 
 
-def test_retrieve_without_id():
-    assert 1 == 2
+def test_retrieve_non_existing(client, admin, restaurant, menu):
+    client = auth_client(client, get_jwt_for_user(admin))
+    resp = client.get(ENDPOINT + f"{menu.id+1000}/")
+
+    assert resp.text == '{"detail":"No Menu matches the given query."}'
+    assert resp.status_code == 404
 
 
-def test_retrieve_by_id():
-    assert 1 == 2
+@pytest.mark.freeze_time("2025-10-16T19:00:00Z")
+def test_retrieve_without_id(client, admin, multiple_menus):
+    client = auth_client(client, get_jwt_for_user(admin))
+    resp = client.get(ENDPOINT)
+
+    assert de_json(resp.text) == MULTIPLE_MENUS_STRUCTURE
+    assert resp.status_code == 200
 
 
-def test_with_today_date():
-    assert 1 == 2
+@pytest.mark.freeze_time("2025-10-16T19:00:00Z")
+def test_with_today_date(client, multiple_menus):
+    resp = client.get(ENDPOINT + "2025-10-16/")
+
+    assert de_json(resp.text) == MULTIPLE_MENUS_STRUCTURE
+    assert resp.status_code == 200
+
+@pytest.mark.freeze_time("2025-10-16T19:00:00Z")
+def test_with_date_in_future(client, multiple_menus):
+    resp = client.get(ENDPOINT + "2026-10-16/")  # 1 year in the future.
+
+    assert resp.status_code == 200
+    assert resp.text == "[]"
 
 
-def test_with_date_in_future():
-    assert 1 == 2
+def test_with_bad_date(client, multiple_menus):
+    resp = client.get(ENDPOINT + "9999-99-99/")
 
-
-def test_with_bad_date():
-    assert 1 == 2
+    assert resp.status_code == 400
+    assert resp.text == '{"details":"Invalid date - \'9999-99-99\'. Correct format is YYYY-MM-DD"}'
