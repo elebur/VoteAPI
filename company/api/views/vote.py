5e52990417a8ef7a125e83b97348b3b77f584aed
elebur <elebur249@proton.me>
2025-10-30 16:48:59 +0200

    style: apply Ruff's suggestions for views/vote.py

--- company/api/views/vote.py
+++ company/api/views/vote.py
@@ -33,7 +33,7 @@ def get_vote_results(request: Request) -> Response:  # noqa: ARG001
             "menu_id": menu.id,
             "likes": likes,
             "dislikes": dislikes,
-            "result": diff
+            "result": diff,
         })
     return Response(result)
 
@@ -63,10 +63,12 @@ def do_vote(request: Request, menu_id: int) -> Response:
             err_msg = (f"You've already {action} this menu '{menu.title}'.")
             result = {"details": err_msg}
         else:
-            vote = Vote.objects.create(menu=menu, employee=emp, like=serializer.data["like"]) # type: ignore
+            vote = Vote.objects.create(
+                menu=menu, employee=emp, like=serializer.data["like"],  # pyright: ignore[reportArgumentType, reportCallIssue]
+            )
             action = "liked" if vote.like else "disliked"
             status_code = status.HTTP_200_OK
-            result = {"vote_id": vote.id, "action": action} # type: ignore
+            result = {"vote_id": vote.id, "action": action}
     else:
         status_code = status.HTTP_400_BAD_REQUEST
         result = {"details": serializer.errors}
