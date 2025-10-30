b0492a905c67135692b96539084dd6ad01a44dd7
elebur <elebur249@proton.me>
2025-10-30 15:36:40 +0200

    style: apply Ruff's suggestions to views/restaurant

--- company/api/views/restaurant.py
+++ company/api/views/restaurant.py
@@ -1,17 +1,22 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.response import Response
-from rest_framework.request import Request
 from rest_framework import status
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from rest_framework.permissions import IsAdminUser
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from base.models import Restaurant
 from api.serializers import RestaurantSerializer
+from base.models import Restaurant
 
 
 @api_view(["GET"])
-def get_restaurant(request: Request, pk: int):
+def get_restaurant(request: Request, pk: int) -> Response:  # noqa: ARG001
+    """Return a Restaurant for the given pk."""
     r = get_object_or_404(Restaurant, pk=pk)
     serializer = RestaurantSerializer(r, many=False)
     return Response(serializer.data)
@@ -20,13 +25,14 @@ def get_restaurant(request: Request, pk: int):
 @api_view(["POST"])
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAdminUser])
-def create_restaurant(request: Request):
+def create_restaurant(request: Request) -> Response:
+    """Add new Restaurant to the DB."""
     serializer = RestaurantSerializer(data=request.data)
 
     if serializer.is_valid():
         serializer.save()
         status_code = status.HTTP_201_CREATED
-        result = {"restaurant_id": serializer.data["id"]} # type: ignore
+        result = {"restaurant_id": serializer.data["id"]} # pyright: ignore[reportArgumentType, reportCallIssue]
     else:
         status_code = status.HTTP_400_BAD_REQUEST
         result = {"details": serializer.errors}
