fea0063cb7b695811331be921548b5bf5f236b0d
elebur <elebur249@proton.me>
2025-10-30 14:48:52 +0200

    style: apply Ruff's suggestions to views/employee

--- company/api/views/employee.py
+++ company/api/views/employee.py
@@ -1,19 +1,24 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.response import Response
-from rest_framework.request import Request
 from rest_framework import status
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from rest_framework.permissions import IsAdminUser
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from base.models import Employee
 from api.serializers import EmployeeSerializer
+from base.models import Employee
 
 
 @api_view(["GET"])
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAdminUser])
-def get_employee(request: Request, pk: int):
+def get_employee(request: Request, pk: int) -> Response:  # noqa: ARG001
+    """Return Employee by its 'pk'."""
     e = get_object_or_404(Employee, pk=pk)
     serializer = EmployeeSerializer(e, many=False)
     return Response(serializer.data)
@@ -22,13 +27,14 @@ def get_employee(request: Request, pk: int):
 @api_view(["POST"])
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAdminUser])
-def add_employee(request: Request):
+def add_employee(request: Request) -> Response:
+    """Add new Employee to the DB."""
     serializer = EmployeeSerializer(data=request.data)
 
     if serializer.is_valid():
         serializer.save()
         status_code = status.HTTP_201_CREATED
-        result = {"employee_id": serializer.data["id"]} # type: ignore
+        result = {"employee_id": serializer.data["id"]} # pyright: ignore[reportArgumentType, reportCallIssue]
     else:
         status_code = status.HTTP_400_BAD_REQUEST
         result = {"details": serializer.errors}
